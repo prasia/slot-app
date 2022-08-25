ce096cf524018441ab50f9764567ea2e2a437ec0
Prasi Aravind <prasi040707@gmail.com>
2022-08-24 17:40:57 -0700

    Created end system for the game + display message when the user has no remaining credits.

--- slots-swiftui/ContentView.swift
+++ slots-swiftui/ContentView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var creditAmt = 1000
+    @State private var creditAmt = 500
 //    @State private var image1 = "cherry"
 //    @State private var image2 = "apple"
 //    @State private var image3 = "star"
     @State private var slotOneFinished = "cherry"
     @State private var slotTwoFinished = "apple"
     @State private var slotThreeFinished = "star"
+    @State private var statusUpdate = String()
     
     
     var body: some View {
@@ -24,6 +25,9 @@ struct ContentView: View {
             Spacer()
             Text("Your Credits: " + String(creditAmt))
             Spacer()
+            Text(statusUpdate).padding()
+                .foregroundColor(Color(red: 1.0, green: 0.25, blue: 0.35, opacity: 1.0)).cornerRadius(15)
+            Spacer()
             HStack{
                 Image(slotOneFinished).resizable().aspectRatio(contentMode: .fit)
                 Image(slotTwoFinished).resizable().aspectRatio(contentMode: .fit)
@@ -32,6 +36,8 @@ struct ContentView: View {
             Spacer()
             Button(action: {
                 let images = ["apple", "cherry", "star"]
+                let winDiff = 100
+                let loseDiff = 25
                 //If all three images are the same, credits += 100, else, credits -= 25
 //                let imgPrefix = "image"
 //
@@ -39,25 +45,28 @@ struct ContentView: View {
                 //                 slotOneFinished = imgPrefix + String(spinOne)
                 //                 slotTwoFinished = imgPrefix + String(spinTwo)
                 //                 slotThreeFinished = imgPrefix + String(spinThree)
-                let spinOne = Int.random(in: 0...2)
-                let spinTwo = Int.random(in: 0...2)
-                let spinThree = Int.random(in: 0...2)
-                
-                slotOneFinished = images[spinOne]
-                slotTwoFinished = images[spinTwo]
-                slotThreeFinished = images[spinThree]
-                
-                if (spinOne == spinTwo && spinTwo == spinThree){
-                    creditAmt += 100
-                }
+                if (creditAmt-25 >= 0){
+                    let spinOne = Int.random(in: 0...2)
+                    let spinTwo = Int.random(in: 0...2)
+                    let spinThree = Int.random(in: 0...2)
+                    
+                    slotOneFinished = images[spinOne]
+                    slotTwoFinished = images[spinTwo]
+                    slotThreeFinished = images[spinThree]
+                    
+                    if (spinOne == spinTwo && spinTwo == spinThree){
+                        creditAmt += winDiff
+                    }
+                    else{
+                        creditAmt -= loseDiff
+                    }}
                 else{
-                    creditAmt -= 25
+                    statusUpdate = "Out of guesses!"
                 }
             
             }) {
                 Text("Spin It").padding()
             }.foregroundColor(.white).background(Color(red: 1.0, green: 0.25, blue: 0.35, opacity: 1.0)).cornerRadius(15)
-            Spacer()
         }
     }
 }
